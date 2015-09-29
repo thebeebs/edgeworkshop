6ba69e13146c9aa0396a1fac8fc3ac3595f46686
thebeebs <martinbeeby@hotmail.com>
2015-09-29 21:56:13 +0100

    Git ignore

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var express = require("express");
 
 var app = express();
 app.use(express.static(__dirname + "/public")); //use static files in ROOT/public folder
-
+console.log(__dirname + "/public")
 app.get("/", function(request, response){ //root dir
     response.send("Hello!!");
 });
